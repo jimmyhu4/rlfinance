c7196790e17138f14ae99c0a0b5541afa4d0f40e
jimmyhu4 <chenhui.hu@gmail.com>
2020-03-21 00:16:13 -0400

    added observation function

--- env/StockTradingEnv.py
+++ env/StockTradingEnv.py
@@ -4,9 +4,13 @@ import numpy as np
 import pandas as pd 
 
 MAX_ACCOUNT_BALANCE = 2147483647 # Use maximum integer
+MAX_NUM_SHARES = 2147483647
+MAX_SHARE_PRICE = 5000
+MAX_OPEN_POSITIONS = 5
+MAX_STEPS = 20000
 
+INITIAL_ACCOUNT_BALANCE = 10000
 N_PREV_DAYS = 5
-
 
 class StockTradingEnv(gym.Env):
     """A stock trading environment for OpenAI gym"""
@@ -25,7 +29,34 @@ class StockTradingEnv(gym.Env):
             low=np.array([0, 0]), high=np.array([3, 1]), dtype=np.float16
         )
 
-        # Open, High, Low, Close prices for the last certain number of days
+        # Open, High, Low, Close prices for the last N days
         self.observation_space = gym.spaces.Box(
             low=0, high=1, shape=(N_PREV_DAYS+1, N_PREV_DAYS+1), dtype=np.float16
         )
+
+        def _next_observation(self):
+            # Get the stock data points for the last N days and scale to between 0-1
+            frame = np.array([
+                self.df.loc[self.current_step: self.current_step +
+                            N_PREV_DAYS, "Open"].values / MAX_SHARE_PRICE,
+                self.df.loc[self.current_step: self.current_step +
+                            N_PREV_DAYS, "High"].values / MAX_SHARE_PRICE,
+                self.df.loc[self.current_step: self.current_step +
+                            N_PREV_DAYS, "Low"].values / MAX_SHARE_PRICE,
+                self.df.loc[self.current_step: self.current_step +
+                            N_PREV_DAYS, "Close"].values / MAX_SHARE_PRICE,
+                self.df.loc[self.current_step: self.current_step +
+                            N_PREV_DAYS, "Volume"].values / MAX_NUM_SHARES,
+            ])
+
+            # Append additional data and scale each value to between 0-1
+            obs = np.append(frame, [[
+                self.balance / MAX_ACCOUNT_BALANCE,
+                self.max_net_worth / MAX_ACCOUNT_BALANCE,
+                self.shares_held / MAX_NUM_SHARES,
+                self.cost_basis / MAX_SHARE_PRICE,
+                self.total_shares_sold / MAX_NUM_SHARES,
+                self.total_sales_value / (MAX_NUM_SHARES * MAX_SHARE_PRICE),
+            ]], axis=0)
+
+            return obs
